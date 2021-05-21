a4aa17cd901e7971fdf2953102ce51607a4aa96f
Anita Lipsky <anita@purplebugs.com>
2021-05-21 15:28:15 +0200

    API key now provided by middleware so it does not leak to client

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const express = require("express");
+const fetch = require("node-fetch");
 const config = require("./config.js");
 
 // create an express app
 const app = express();
+
+// get key
+const baseURL = "https://the-sneaker-database.p.rapidapi.com";
+
+const headers = {
+  "x-rapidapi-key": config.get("sneakersDatabaseAPIKey"),
+  "x-rapidapi-host": "the-sneaker-database.p.rapidapi.com",
+};
+
+const limit = 10;
+
+app.get("/api", async (req, res) => {
+  const response = await fetch(`${baseURL}/sneakers?limit=${limit}`, {
+    method: "GET",
+    headers: headers,
+  });
+
+  if (response.status === 200 || response.status === 304) {
+    const responseJSON = await response.json();
+
+    res.send(JSON.stringify(responseJSON));
+  } else {
+    throw new Error("Unable to fetch data");
+  }
+});
 
 app.use("/", express.static("static"));
 
--- static/functions.js
+++ static/functions.js
@@ -1,25 +1,7 @@
-const baseURL = "https://the-sneaker-database.p.rapidapi.com";
-
-const headers = {
-  "x-rapidapi-key": "TODO",
-  "x-rapidapi-host": "the-sneaker-database.p.rapidapi.com",
-};
-
 const getSneakers = async (limit = 10, priced = true) => {
-  const response = await fetch(`${baseURL}/sneakers?limit=${limit}`, {
-    method: "GET",
-    headers: headers,
-  });
+  const response = await fetch(`/api`);
   if (response.status === 200 || response.status === 304) {
     const responseJSON = response.json();
-
-    // if (priced === true) {
-    //   // only return sneakers with a price
-
-    //   responseJSON.filter((sneaker) => {
-    //     return sneaker.retailPrice > 0;
-    //   });
-    // }
 
     return responseJSON;
   } else {
