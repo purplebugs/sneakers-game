4c5d6b50ed73f2f26a389b69d0d42bc1cb1d3376
Anita Lipsky <anita@purplebugs.com>
2021-05-22 15:46:34 +0200

    render sneakers with image and price only, default first fetch of 100

--- static/functions.js
+++ static/functions.js
@@ -1,7 +1,11 @@
-const getSneakers = async (limit = 10, page = 0) => {
+const getSneakers = async (limit = 100, page = 0) => {
   const response = await fetch(`/api/${limit}/${page}`);
   if (response.status === 200 || response.status === 304) {
     const responseJSON = response.json();
+
+    // store in session storage for later use to avoid calling API again
+    sessionStorage.setItem("sneakersStorage", JSON.stringify(responseJSON));
+
     return responseJSON;
   } else {
     throw new Error("Unable to fetch data");
@@ -16,5 +20,19 @@ const renderSneakers = (sneakers) => {
 
   // render sneakers
   console.log("[APP LOG] sneakers", sneakers);
-  sneakersEl.innerText = JSON.stringify(sneakers);
+
+  // render only sneakers with image and price
+
+  const sneakersWithPriceAndImage = sneakers.results.filter((sneaker) => {
+    const sneakerWithPrice = sneaker.retailPrice > 0;
+    //console.log("[APP LOG] sneakerWithPrice", sneakerWithPrice);
+
+    const sneakerWithImage = sneaker.image.small !== "";
+    //console.log("[APP LOG] sneakerWithImage", sneakerWithImage);
+
+    return sneakerWithPrice && sneakerWithImage;
+  });
+
+  console.log("[APP LOG] sneakersWithPriceAndImage", sneakersWithPriceAndImage);
+  sneakersEl.innerText = JSON.stringify(sneakersWithPriceAndImage);
 };
